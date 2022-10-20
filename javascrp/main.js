cf34eebf8d7c581f1cd277feaa37fe92d6ef6dc3
Mark <caco.burato@gmail.com>
2022-10-20 16:20:53 -0300

    Incorporar Fetch

--- javascrp/main.js
+++ javascrp/main.js
@@ -1,77 +1,89 @@
-const productosMain = JSON.parse(localStorage.getItem("productosJSON"))
-indexProductos(productosMain)
-let input1 = document.getElementById("barra-de-busqueda").addEventListener("input", (e) => {
-  let value = e.target.value.toUpperCase()
-  console.log(value)
-  if (value.length > 0) {
-    mostrarBusqueda(value)
-  } else {
-    indexProductos(productosMain)
-  }
-})
+// const productosMain = JSON.parse(localStorage.getItem("productosJSON"));
+fetch("../productos.json")
+  .then((res) => res.json())
+  .then((productosMain) => {
+    indexProductos(productosMain);
+    let input1 = document
+      .getElementById("barra-de-busqueda")
+      .addEventListener("input", (e) => {
+        let value = e.target.value.toUpperCase();
+        console.log(value);
+        if (value.length > 0) {
+          mostrarBusqueda(value);
+        } else {
+          indexProductos(productosMain);
+        }
+      });
 
-function mostrarBusqueda(el) {
-  const busqueda = productosMain.filter((a) => a.nombre.includes(el))
-  const productoBusqueda = document.getElementById("productos_id_index")
-  productoBusqueda.innerHTML = ""
-  insertProducts(busqueda, productoBusqueda)
-}
-
-function indexProductos(prod) {
-  const productosIndex = document.getElementById("productos_id_index")
-  productosIndex.innerHTML = ""
-  insertProducts(prod, productosIndex)
-}
-
-function insertProducts(prod, product) {
-  let cont = 1
-  let contClase = 0
-  for (const producto of prod) {
-    let contClaseString = "div_padre" + contClase
-    let contenedor = document.createElement("div")
-    if (producto.prodCar == 1) {
-      switch (cont) {
-        case 1:
-          contenedor.setAttribute("id", contClaseString)
-          contenedor.classList.add("row", "mx-auto")
-          contenedor.innerHTML = `<div class="col-sm-8 col-md-6 col-xl-6 col-10 mx-auto pb-3">
-                              <article>
-                              <h2>
-                              <a href=${producto.direc}><img src=${producto.imgFile} alt=${producto.alt} id=productoCarrito${producto.id} class="rounded img-fluid shadow"></a>
-                              </h2>
-                              </article>
-                              </div>`
-          cont++
-          product.appendChild(contenedor)
-          break;
-        case 2:
-          const divPadre = document.getElementById(contClaseString)
-          contenedor.classList.add("col-sm-8", "col-md-6", "col-xl-6", "col-10", "mx-auto", "pb-3")
-          contenedor.innerHTML = `
-                              <article>
-                              <h2>
-                              <a href=${producto.direc}><img src=${producto.imgFile} alt=${producto.alt} id=productoCarrito${producto.id} class="rounded img-fluid shadow"></a>
-                              </h2>
-                              </article>`
-          cont--
-          divPadre.appendChild(contenedor)
-          contClase++
-          break;
-      }
-    } else {
-
-      contenedor.setAttribute("id", contClaseString)
-      contenedor.classList.add("row", "mx-auto")
-      contenedor.innerHTML = `<div class="col-12 mx-auto pb-3">
-                              <article>
-                              <h2>
-                              <a href=${producto.direc}><img src=${producto.imgFile} alt=${producto.alt} id=productoCarrito${producto.id} class="rounded img-fluid shadow"></a>
-                              </h2>
-                              </article>
-                              </div>`
-      contClase++
-      product.appendChild(contenedor)
-      cont = 1
+    function mostrarBusqueda(el) {
+      const busqueda = productosMain.filter((a) => a.nombre.includes(el));
+      const productoBusqueda = document.getElementById("productos_id_index");
+      productoBusqueda.innerHTML = "";
+      insertProducts(busqueda, productoBusqueda);
     }
-  }
-}
+
+    function indexProductos(prod) {
+      const productosIndex = document.getElementById("productos_id_index");
+      productosIndex.innerHTML = "";
+      insertProducts(prod, productosIndex);
+    }
+
+    function insertProducts(prod, product) {
+      let cont = 1;
+      let contClase = 0;
+      for (const producto of prod) {
+        let contClaseString = "div_padre" + contClase;
+        let contenedor = document.createElement("div");
+        if (producto.prodCar == 1) {
+          switch (cont) {
+            case 1:
+              contenedor.setAttribute("id", contClaseString);
+              contenedor.classList.add("row", "mx-auto");
+              contenedor.innerHTML = `<div class="col-sm-8 col-md-6 col-xl-6 col-10 mx-auto pb-3">
+                              <article>
+                              <h2>
+                              <a href=${producto.direc}><img src=${producto.imgFile} alt=${producto.alt} id=productoCarrito${producto.id} class="rounded img-fluid shadow"></a>
+                              </h2>
+                              </article>
+                              </div>`;
+              cont++;
+              product.appendChild(contenedor);
+              break;
+            case 2:
+              const divPadre = document.getElementById(contClaseString);
+              contenedor.classList.add(
+                "col-sm-8",
+                "col-md-6",
+                "col-xl-6",
+                "col-10",
+                "mx-auto",
+                "pb-3"
+              );
+              contenedor.innerHTML = `
+                              <article>
+                              <h2>
+                              <a href=${producto.direc}><img src=${producto.imgFile} alt=${producto.alt} id=productoCarrito${producto.id} class="rounded img-fluid shadow"></a>
+                              </h2>
+                              </article>`;
+              cont--;
+              divPadre.appendChild(contenedor);
+              contClase++;
+              break;
+          }
+        } else {
+          contenedor.setAttribute("id", contClaseString);
+          contenedor.classList.add("row", "mx-auto");
+          contenedor.innerHTML = `<div class="col-12 mx-auto pb-3">
+                              <article>
+                              <h2>
+                              <a href=${producto.direc}><img src=${producto.imgFile} alt=${producto.alt} id=productoCarrito${producto.id} class="rounded img-fluid shadow"></a>
+                              </h2>
+                              </article>
+                              </div>`;
+          contClase++;
+          product.appendChild(contenedor);
+          cont = 1;
+        }
+      }
+    }
+  });
--- javascrp/productpages.js
+++ javascrp/productpages.js
@@ -1,54 +1,58 @@
 import {
   validarProductoRepetido,
-  mostrarCarrito
-} from "./components/products/cartActions.js"
+  mostrarCarrito,
+} from "./components/products/cartActions.js";
 
-const prod = JSON.parse(localStorage.getItem("productosJSON"))
-const contador = document.querySelector(".count")
-let cont = 1
-const suma = document.querySelector(".plus")
-const resta = document.querySelector(".minus")
-contador.innerHTML = cont
-suma.addEventListener("click", () => {
-  if (cont >= 10) {
-    contador.innerHTML = "10"
-  } else {
-    cont++
-    contador.innerHTML = cont
-  }
-})
-resta.addEventListener("click", () => {
-  if (cont <= 1) {
-    contador.innerHTML = 1
-  } else {
-    cont--
-    contador.innerHTML = cont
-  }
-})
+fetch("../productos.json")
+  .then((res) => res.json())
+  .then((prod) => {
+    console.log(prod);
+    // const prod = JSON.parse(localStorage.getItem("productosJSON"));
+    const contador = document.querySelector(".count");
+    let cont = 1;
+    const suma = document.querySelector(".plus");
+    const resta = document.querySelector(".minus");
+    contador.innerHTML = cont;
+    suma.addEventListener("click", () => {
+      if (cont >= 10) {
+        contador.innerHTML = "10";
+      } else {
+        cont++;
+        contador.innerHTML = cont;
+      }
+    });
+    resta.addEventListener("click", () => {
+      if (cont <= 1) {
+        contador.innerHTML = 1;
+      } else {
+        cont--;
+        contador.innerHTML = cont;
+      }
+    });
 
-let carrito = JSON.parse(sessionStorage.getItem("carritoJSON"))
-if (carrito.length !== 0) {
-  carrito.forEach(e => {
-    mostrarCarrito(e)
+    let carrito = JSON.parse(sessionStorage.getItem("carritoJSON"));
+    if (carrito.length !== 0) {
+      carrito.forEach((e) => {
+        mostrarCarrito(e);
+      });
+    }
+
+    const seleccionProducto = document.querySelector(".button_carrito");
+    const title = document.querySelector("#titlePages").innerHTML;
+    seleccionProducto.addEventListener("click", () => {
+      const busqueda = prod.find((a) => a.nombre === title);
+      validarProductoRepetido(busqueda, cont);
+
+      Toastify({
+        text: "Se agrego un producto al carrito",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+      }).showToast();
+    });
   });
-}
-
-const seleccionProducto = document.querySelector(".button_carrito")
-const title = document.querySelector("#titlePages").innerHTML
-seleccionProducto.addEventListener("click", () => {
-
-  const busqueda = prod.find((a) => a.nombre === title)
-  validarProductoRepetido(busqueda, cont)
-
-  Toastify({
-    text: "Se agrego un producto al carrito",
-    duration: 3000,
-    close: true,
-    gravity: "top",
-    position: "center",
-    stopOnFocus: true,
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-  }).showToast();
-})
